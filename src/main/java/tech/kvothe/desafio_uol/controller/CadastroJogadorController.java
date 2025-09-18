28b88e70f223234cfbc89db47963e9f69cd1535b
Ítalo H. <italodssouza.backup@gmail.com>
2025-09-18 12:38:41 -0300

    adding @GetMapping to redirect to the cadastrar-jogador page

--- src/main/java/tech/kvothe/desafio_uol/controller/CadastroJogadorController.java
+++ src/main/java/tech/kvothe/desafio_uol/controller/CadastroJogadorController.java
@@ -1,9 +1,12 @@
 package tech.kvothe.desafio_uol.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tech.kvothe.desafio_uol.model.GrupoCodinome;
 import tech.kvothe.desafio_uol.model.Jogador;
 import tech.kvothe.desafio_uol.service.JogadorService;
 
@@ -24,5 +27,12 @@ public class CadastroJogadorController {
         } catch (Exception e) {
             return "redirect:/cadastro-jogador";
         }
+    }
+
+    @GetMapping
+    public String paginaCadastroogador(Model model) {
+        model.addAttribute("jogador", new Jogador(null,null,null,null,null));
+        model.addAttribute("gruposCodinomes", GrupoCodinome.values());
+        return "cadastro_jogador";
     }
 }
